83c643249836c8e5cf30afb8cfd4b6e6522ced13
seok <yae06067@gmail.com>
2023-04-27 09:17:37 +0900

    BridgeTruck

--- 지석현/daily/OrdinaryBackpack.py
+++ 지석현/daily/OrdinaryBackpack.py
@@ -4,47 +4,36 @@
 #   - 들고갈 물건 후보들 N개 중, 각 물건의 무게(W)와 가치(V)의 여러 경우의 수를 판별
 #   - 제한된 무게(K)이하인 물건(들)의 최대 가치 구하기
 # 
-# 관련 알고리즘 : 냅색(Knapsack) 알고리즘_0-1 Knapsack
-
-
-# n, k = map(int, input().split())
-
-# stuff = [[0,0]]
-# DP = [[0] * (k+1) for _ in range(n+1)]
-
-# for _ in range(n):
-#     stuff.append(list(map(int, input().split())))
-
-# for i in range(1,n+1):
-#     for j in range(1,k+1):
-#         w,v = stuff[i]
-#         if j >= w:
-#             DP[i][j] = max(DP[i-1][j] , DP[i-1][j-w] + v)
-#         else:
-#             DP[i][j] = DP[i-1][j]
-
-# print(DP[i][k])
-
+# 관련 알고리즘 : 냅색(Knapsack) 알고리즘
+#                   - Fraction Knapsack: 나눌 수 있는 물건에 문제 즉, 무게 대비 가치를 구해 해결 -> greedy 
+#                   - 0-1 Knapsack: 나눌 수 없는 물건에 대한 문제 -> 이번 문제에 적용할 알고리즘
 
 n, k = map(int,input().split()) # 물건 개수(n)와 들 수 있는 최대 무게(k)
 items = [[0, 0]]  # 물건 번호와 가치 저장 리스트
-dp = [[0 for _ in range(k+1)] for _ in range(n+1)] # 경우의 수 리스트
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)] # (가방 최대 무게+1) * (물건 개수+1) 리스트 생성
 
-for i in range(n): # 물건의 개수만큼 무게와 가중치 각각 입력 받기
-     items.append(list(map(int, input().split())))
+
+# 물건의 개수만큼 무게와 가중치 각각 리스트 형식으로 입력 받아 넣기
+for i in range(n): 
+    items.append(list(map(int, input().split())))
 
 # 냅색 알고리즘
 for i in range(1, n+1):
-    w, v = items[i]
+    w, v = items[i] # items[i][]
     for j in range(1, k+1):
-        
-
+        # 공간이 있을 때
         if j >= w:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v)
-        else:
-            dp[i][j] = dp[i-1][j]
+            # 배낭 최대 용량 j 일 때, i번째 물건을 넣을려는 상황 = 이전 가치값 vs i번째 물건 넣었을 때 가치값
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w]+v) 
 
-print(dp[n][k])
+        # 공간이 없으면 그냥 이전 값 고대로 계승
+        else: 
+            # 배낭 최대 용량 j 일 때, i-1번째 물건까지 확인한 최대 가치값
+            dp[i][j] = dp[i-1][j] 
+
+# k: 마지막까지 왔을 때의 값 = 최대 가치 값
+print(dp[n][k]) 
 
 
-
+# 살펴보면 좋은 사이트
+# https://chanhuiseok.github.io/posts/improve-6/ - 냅색 알고리즘 기반 2차원 배열 풀이 + 1차원 배열 풀이
---
+++ 지석현/daily/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+if s[0] == '-':
+#     del s[0]
+#     answer = int(s) * -1
